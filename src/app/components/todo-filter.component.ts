0296e9e93898438d057e5f219be81a6c0e60a99f
mohamedtatahdev <mohamed.tatah69@gmail.com>
2025-08-14 12:20:58 +0200

    first commit

---
+++ src/app/components/todo-filter.component.ts
@@ -0,0 +1,20 @@
+import {Component, model} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-todo-filter',
+  imports: [FormsModule],
+  template: `
+    <input
+      type="text"
+      [(ngModel)]="filter"
+      class="border py-6"
+      placeholder="Rechercher"
+    />
+  `,
+  styles: ``
+})
+export class TodoFilterComponent {
+  filter = model<string>('');
+
+}
--- src/app/components/todos-list.component.ts
+++ src/app/components/todos-list.component.ts
@@ -1,14 +1,21 @@
-import {Component, effect, input, output} from '@angular/core';
+import {Component, computed, effect, input, output, signal} from '@angular/core';
 import { TodoComponent } from './todo.component';
 import {Todo} from '../shared/interfaces';
+import {TodoFilterComponent} from './todo-filter.component';
 
 @Component({
   selector: 'app-todos-list',
-  imports: [ TodoComponent],
+  imports: [TodoComponent, TodoFilterComponent],
   template: `
+    <hr />
+    <app-todo-filter [(filter)]="filter"  />
+    <hr />
+    <hr />
+    <strong>Nombre de todos : {{ todosLength() }}</strong>
+    <hr />
     <ul class="flex flex-col gap-12">
 
-      @for ( todo of todosList() ; track todo.id) {
+      @for ( todo of filteredTodosList() ; track todo.id) {
         <app-todo (toggleTodo)="toggleTodo.emit($event)" [todo]="todo" />
       } @empty {
         <li>Il n'y a pas de todo pour l'instant</li>
@@ -22,8 +29,14 @@ import {Todo} from '../shared/interfaces';
   `,
 })
 export class TodosListComponent {
-    todosList = input<Todo[]>([])
-    toggleTodo = output<string>()
+    filter = signal<string>('');
+    todosList = input<Todo[]>([]);
+    toggleTodo = output<string>();
+  todosLength  = computed(() => this.filteredTodosList()?.length);
+
+    filteredTodosList = computed(() =>
+        this.todosList().filter((t) => t.name.toLowerCase().includes(this.filter()))
+      );
 
 
   //debug
